329ddb6726bd4275d4a4d1d54e0f362304074fb9
学少何 <a664554724@qq.com>
2023-06-03 23:31:28 +0800

    [comment dao] fix bug after remove `comment_table.reply_to`

--- backend/src/dao/comment.rs
+++ backend/src/dao/comment.rs
@@ -1,8 +1,7 @@
 use chrono::{Local, NaiveDateTime};
 use mysql::{
     params,
-    prelude::{FromRow, Queryable},
-    Value,
+    prelude::{FromRow, Queryable}
 };
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -10,36 +9,25 @@ use uuid::Uuid;
 use super::{connect::get_conn, dao_error::DaoError, get_value, user::UserSimplify};
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct CommentReplyView {
-    pub comment_id: i32,
-    pub user: UserSimplify,
-    pub comment_content: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
 pub struct CommentView {
     pub comment_id: i32,
     pub user: UserSimplify,
     pub comment_time: NaiveDateTime,
     pub comment_content: String,
-    pub reply_content: Option<CommentReplyView>,
 }
 
 impl FromRow for CommentView {
     /// This search on database, need row like this, the column count is 11 at least:
     /// ```sql
-    /// select cut.comment_id, cut.user_name, cut.user_nickname, cut.avatar, cut.comment_time, cut.comment_content,
-    ///        cut.reply_to, cut2.user_name, cut.user_nickname, cut2.avatar, cut2.comment_content
+    /// select cut.comment_id, cut.user_name, cut.user_nickname, cut.avatar, cut.comment_time, cut.comment_content
     /// ```
     ///
     /// `cut.comment_id, cut.user_name, cut...` is this comment information.
-    ///
-    /// `cut.reply_to, cut2.user_name, cut.user_ni...` is comment what this comment reply to.
     fn from_row_opt(row: mysql::Row) -> Result<Self, mysql::FromRowError>
     where
         Self: Sized,
     {
-        if row.len() < 11 {
+        if row.len() < 6 {
             return Err(mysql::FromRowError(row));
         }
 
@@ -52,18 +40,6 @@ impl FromRow for CommentView {
             },
             comment_time: get_value(&row[4], &row)?,
             comment_content: get_value(&row[5], &row)?,
-            reply_content: match row[6] != Value::NULL {
-                true => Some(CommentReplyView {
-                    comment_id: get_value(&row[6], &row)?,
-                    user: UserSimplify {
-                        user_name: get_value(&row[7], &row)?,
-                        user_nickname: get_value(&row[8], &row)?,
-                        avatar: get_value(&row[9], &row)?,
-                    },
-                    comment_content: get_value(&row[10], &row)?,
-                }),
-                false => None,
-            },
         })
     }
 }
@@ -72,18 +48,12 @@ impl CommentView {
     pub fn get_comments(post_id: i32, offset: i32, count: i32) -> Result<Vec<Self>, DaoError> {
         let mut conn = get_conn();
 
-        let sql = r"select cut.comment_id, cut.user_name, cut.user_nickname, cut.avatar, cut.comment_time, cut.comment_content, 
-            cut.reply_to, cut2.user_name, cut.user_nickname, cut2.avatar, cut2.comment_content
+        let sql = r"select cut.comment_id, cut.user_name, cut.user_nickname, cut.avatar, cut.comment_time, cut.comment_content
         from (
-          select ct.comment_id, ct.post_id, ct.user_id, ct.reply_to, ct.comment_time, ct.comment_content, ut.user_name, ut.user_nickname, ut.avatar 
+          select ct.comment_id, ct.post_id, ct.user_id, ct.comment_time, ct.comment_content, ut.user_name, ut.user_nickname, ut.avatar 
           from comment_table ct 
           left join user_table ut on ct.user_id=ut.user_id
         ) cut
-        left join (
-          select ct.comment_id, ct.comment_content, ut.user_name, ut.user_nickname, ut.avatar 
-          from comment_table ct 
-          left join user_table ut on ct.user_id=ut.user_id
-        ) cut2 on cut2.comment_id=cut.reply_to
         where cut.post_id=:post_id
         order by cut.comment_time
         limit :offset, :count";
@@ -120,17 +90,15 @@ pub struct CommentPutRequest {
     pub post_id: i32,
     pub token: Uuid,
     pub comment_content: String,
-    pub reply_to: Option<i32>,
 }
 
 impl CommentPutRequest {
     pub fn put_comment(&self) -> Result<(), DaoError> {
         let mut conn = get_conn();
 
-        let sql = r"insert comment_table(post_id,user_id,reply_to,comment_time,comment_content)
+        let sql = r"insert comment_table(post_id,user_id,comment_time,comment_content)
             values (:post_id, 
                 (select ut.user_id from user_table ut where ut.token=:token), 
-                :reply_to, 
                 :now_time,
                 :comment_content
             )";
@@ -138,7 +106,6 @@ impl CommentPutRequest {
         let param = params! {
             "post_id"=>self.post_id,
             "token"=>self.token,
-            "reply_to"=>self.reply_to,
             "now_time"=>Local::now().naive_local(),
             "comment_content"=>&self.comment_content
         };
@@ -170,7 +137,6 @@ pub struct CommentDb {
     pub comment_id: i32,
     pub post_id: i32,
     pub user_id: u32,
-    pub replay_to: Option<i32>,
     pub comment_time: NaiveDateTime,
     pub comment_content: String,
 }
